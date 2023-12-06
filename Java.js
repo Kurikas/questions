f4a33e507bd6aaf2bc20ba15005538137cef6bdf
Kurikas <115626417+Kurikas@users.noreply.github.com>
2023-12-06 16:11:57 +0200

    Update Java.js

--- Java.js
+++ Java.js
@@ -1,62 +1,36 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer'
 
-const questions = [
-    {
-        type: 'list',
-        name: 'capitalOfFrance',
-        message: 'What is the capital of France?',
-        choices: ['Berlin', 'London', 'Paris', 'Madrid'],
-        default: 'Paris'
-    },
-    {
-        type: 'list',
-        name: 'currencyOfItaly',
-        message: 'What is the currency of Italy?',
-        choices: ['Euro', 'Dollar', 'Pound', 'Yen'],
-        default: 'Euro'
-    },
-    {
-        type: 'list',
-        name: 'languageOfSpain',
-        message: 'What is the official language of Spain?',
-        choices: ['English', 'Spanish', 'French', 'German'],
-        default: 'Spanish'
-    },
-    {
-        type: 'list',
-        name: 'capitalOfGermany',
-        message: 'What is the capital of Germany?',
-        choices: ['Berlin', 'Paris', 'Rome', 'Madrid'],
-        default: 'Berlin'
-    },
-    {
-        type: 'list',
-        name: 'currencyOfSpain',
-        message: 'What is the currency of Spain?',
-        choices: ['Euro', 'Dollar', 'Pound', 'Yen'],
-        default: 'Euro'
-    },
-    {
-        type: 'list',
-        name: 'languageOfItaly',
-        message: 'What is the official language of Italy?',
-        choices: ['English', 'Italian', 'French', 'Spanish'],
-        default: 'Italian'
-    },
-    {
-        type: 'list',
-        name: 'capitalOfSpain',
-        message: 'What is the capital of Spain?',
-        choices: ['Berlin', 'London', 'Paris', 'Madrid'],
-        default: 'Madrid'
-    },
-];
-
-
-inquirer.prompt(questions)
-    .then((answers) => {
-        console.log('Answers:', answers);
-    })
-    .catch((error) => {
-        console.error('Error occurred:', error);
-    });
+inquirer.prompt([
+  {
+    name: "Nimi",
+    message: "Kuuled vä,mis su nimi on?",
+    type: "input",
+  },
+  {
+    name: "Rajoonid",
+    message: "Mis rajoon sulle kõige vähem meeldib?",
+    type: "list",
+    choices: ["Mustamäe", "Õismäe", "Kesklinn", "Lasnamäe", "Kopli", "Viimsi"]
+  },
+  {
+    name: "Vanus",
+    message: "Vana sa tatt oled?",
+    type: "number",
+  },
+  {
+    name: "Vabaaeg",
+    message: "Mis muidu teed?",
+    type: "checkbox",
+    choices: ["Tobi", "Chillin", "Pelan", "Vihakõne", "Joon", "Kaklen"]
+  },
+  {
+    name: "Kinnitus",
+    message: "Oled kõva vend jah?",
+    type: "confirm",
+  },
+  {
+    name: "Viimane",
+    message: "Keda sa enim vihkad?",
+    type: "password",
+  }
+])
